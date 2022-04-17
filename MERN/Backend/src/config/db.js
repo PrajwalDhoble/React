a137229c568a75ce1dd18b67800b8ceb49de65eb
PrajwalDhoble <prsjwaldhoble2000@gmail.com>
2022-04-17 18:12:22 +0530

    pass added

--- MERN/Backend/src/config/db.js
+++ MERN/Backend/src/config/db.js
@@ -2,7 +2,7 @@ const mongoose =require("mongoose");
 
 const connect=()=>{
     return mongoose.connect(
-        "mongodb+srv://prajwal:<password>@cluster0.b0wgc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+        "mongodb+srv://prajwal:prajwal_123@cluster0.b0wgc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
             )
 };
 module.exports=connect
