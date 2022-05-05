110cbe33fd93e2988238387097a928a0b128e34b
PrajwalDhoble <prsjwaldhoble2000@gmail.com>
2022-05-05 19:59:47 +0530

    stopwatch

---
+++ stopwatch/stopwatch/src/App.jsx
@@ -0,0 +1,52 @@
+// import { useState } from 'react'
+// import logo from './logo.svg'
+import './App.css'
+import React from 'react'
+
+function App() {
+  const [sec, setSec] = React.useState(0)
+  const [min, setMin] = React.useState(0)
+  const [hr, setHr] = React.useState(0);
+  const [status, setStatus] = React.useState(false);
+
+  React.useEffect(() => {
+    var id = null;
+
+    if (sec > 59) {
+      setMin(min + 1);
+      setSec(0)
+    }
+
+    if (min > 59) {
+      setHr(hr + 1);
+      setMin(0);
+    }
+
+
+    if (status) {
+      id = setInterval(() => {
+        setSec((sec) => sec + 1)
+      }, 1000)
+    }
+    else {
+      clearInterval(id);
+    }
+
+    return (() => clearInterval(id))
+  })
+
+  return (
+    <div>
+      <div>
+        {hr} Hr: {min} Min: {sec} Seconds
+      </div>
+      <div>
+        <button onClick={() => setStatus(true)}>Start</button>
+        <button onClick={() => setStatus(false)}>Pause</button>
+        <button onClick={() => { setStatus(false); setHr(0); setSec(0); setMin(0) }}>Reset</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
