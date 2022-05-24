480382c61ca010cd5502d602bf5646da5a6f0d79
PrajwalDhoble <prsjwaldhoble2000@gmail.com>
2022-05-24 15:10:19 +0530

    new project added

---
+++ arraymethods/arrays/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+   <div>
+     <button></button><button></button><button></button><button></button><button></button><button></button>
+   </div>
+  )
+}
+
+export default App
